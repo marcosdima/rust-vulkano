2f1169cfd503e971c489fae1ca743170e078bdaf
marcosdima <marcosdimatteo.dev@gmail.com>
2025-01-31 20:55:41 -0300

    challenge completed!

--- tutorial/src/lib.rs
+++ tutorial/src/lib.rs
@@ -13,6 +13,8 @@ struct State<'a> {
     size: winit::dpi::PhysicalSize<u32>,
     window: &'a Window,
     render_pipeline: wgpu::RenderPipeline,
+    challenge_render_pipeline: wgpu::RenderPipeline, 
+    default_color: bool,
 }
 
 impl<'a> State<'a> {
@@ -128,6 +130,51 @@ impl<'a> State<'a> {
             }
         );
 
+        // Secondary shader.
+        let shader = device.create_shader_module(wgpu::include_wgsl!("challenge.wgsl"));
+
+        let challenge_render_pipeline = device.create_render_pipeline(
+            &wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main",
+                    buffers: &[],
+                    compilation_options: wgpu::PipelineCompilationOptions::default(),
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: config.format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                    compilation_options: wgpu::PipelineCompilationOptions::default(),
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    unclipped_depth: false,
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                multiview: None,
+                cache: None,
+            }
+        );
+
+        let default_color = false;
+
         Self {
             surface,
             device,
@@ -136,6 +183,8 @@ impl<'a> State<'a> {
             size,
             window,
             render_pipeline,
+            challenge_render_pipeline,
+            default_color,
         }
     }
 
@@ -153,7 +202,23 @@ impl<'a> State<'a> {
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
-        false // for now...
+        match event {
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        state,
+                        physical_key: PhysicalKey::Code(KeyCode::Space),
+                        ..
+                    },
+                ..
+            } => {
+                if *state == ElementState::Released {
+                    self.default_color = !self.default_color;
+                }
+                true
+            }
+            _ => false,
+        }
     }
 
     fn update(&mut self) {
@@ -196,7 +261,14 @@ impl<'a> State<'a> {
                 }
             );
         
-            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_pipeline(
+                if self.default_color {
+                    &self.render_pipeline
+                } else {
+                    &self.challenge_render_pipeline
+                }
+            );
+            
             render_pass.draw(0..3, 0..1);
         }
     
